579626240503ca2ba1055887f63448d44506ca7a
MarcoPizarro-LT <apizarro74@hotmail.com>
2017-09-16 04:44:05 -0500

    V01.001

---
+++ src/main/java/com/spring/mua/bean/deposito.java
@@ -0,0 +1,24 @@
+package com.spring.mua.bean;
+
+public class deposito {
+
+	private String codigo;
+	private String nombre;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
